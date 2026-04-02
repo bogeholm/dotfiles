23263278118bf4db08f5c14dae58da398af78140
@bogeholm <troelsmikkelsen@gmail.com>
2026-04-02 21:11:25 +0200

    Cumulative update 2026 Q2

--- bin/executable_update.sh
+++ bin/executable_update.sh
@@ -6,10 +6,6 @@ brew bundle install --file ~/macos/Brewfile
 brew upgrade
 brew cleanup
 
-conda update -n base -c conda-forge conda --yes
-
-pipx upgrade-all
-
 rustup self update
 rustup update stable
 
@@ -17,13 +13,6 @@ if which cargo-install-update 2>&1 > /dev/null; then
   cargo install-update -a
 fi
 
-tldr --update
-
-pipx list --short | cut -d ' ' -f 1 > ~/macos/pipx-packages.txt
-
-# Update App Store apps
-mas upgrade
-# List OS upgrades to install manually
 softwareupdate --list
 
 chezmoi diff
